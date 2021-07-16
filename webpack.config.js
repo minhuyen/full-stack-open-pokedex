f3b37e43261e98c294bd34b275d1ad3123a14ddd
Uyen Do <minhuyendo@gmail.com>
2021-07-16 14:04:36 +0700

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        loaders: ['style-loader', 'css-loader'],
       }
     ],
   },
